14580f7d665d9dd44d6f4ee85ca1e6602fe010b5
karrow03 <karrow03@gmail.com>
2022-10-20 09:05:25 +0200

    index i css

---
+++ index/formularz-projekt.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+    <head>
+        <title>PAI</title>
+        <meta charset="utf-8">
+        <link href="style.css" rel="stylesheet" >
+    </head>
+    <body>
+
+<form action="./formularz-projekt.php" method="POST">
+
+
+<h3> Podaj login: </h3>
+<input type="text" name="login"><br>
+<h3> Podaj hasło: </h3>
+<input type="password" name="haslo"><br><br>
+<input type="submit" value="Zaloguj" name="wyslij"><br><br>
+
+
+<hr><br>
+<h1> REJESTRACJA </h1>
+Imię:     <input type="text" name="imie"><br>
+Nazwisko: <input type="text" name="nazwisko"><br>
+Login:    <input type="text" name="login-re"><br>
+Haslo:    <input type="password" name="haslo-re"><br>
+
+
+
+
+</form>
+
+
+<?php
+
+
+
+?>
+
+</body>
